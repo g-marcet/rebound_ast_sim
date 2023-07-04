1f1b70c81816efa71c946e6b070a5322a05ed7aa
g-marcet <gerard.marcet@autonoma.cat>
2023-07-04 12:11:09 +0200

    Changed test particles to synthetic population

--- problem.c
+++ problem.c
@@ -11,37 +11,69 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <math.h>
+#include <string.h>
 #include "rebound.h"
+
+#define FILENAME_SIZE 64
+#define MAX_LINE 256
+#define AST_NUM 10
+
+
+
 
 void heartbeat(struct reb_simulation* r);
 
 int main(int argc, char* argv[]){
     struct reb_simulation* r = reb_create_simulation_from_binary("ss-2020-03-03.bin");
     // Setup constants
-    r->dt           = 4./365.25*2.*M_PI;        // 4days
-    r->integrator   = REB_INTEGRATOR_WHFAST;
-    r->ri_whfast.coordinates   = REB_WHFAST_COORDINATES_DEMOCRATICHELIOCENTRIC;
+    r->dt           = 0.5/365.25*2.*M_PI;        // 0.5 days
     r->heartbeat    = heartbeat;
     r->N_active     = r->N;
+    r->ri_ias15.min_dt       = 0.01;
 
-    // Test Particles
-    for (int i=0;i<10000;i++){
-        double a = reb_random_uniform(r, 0.4,20.);
-        double e = reb_random_uniform(r, 0.01,0.2);
-        double omega = reb_random_uniform(r, 0.,2.*M_PI);
-        double f = reb_random_uniform(r, 0.,2.*M_PI);
-        struct reb_particle p = reb_tools_orbit_to_particle(1.,r->particles[0],0.,a,e,0.,0.,omega,f);
-        reb_add(r, p); 
+    // Read Synthetic Population
+    FILE *fp;
+    char buffer[MAX_LINE];
+
+ 
+
+    fp = fopen("asteroids.cat", "r");
+    if (fp == NULL){
+        perror("Error");
+        return(-1);
+    } 
+    for (int i=0; i<AST_NUM; i++){
+    
+        fgets(buffer, MAX_LINE, fp);
+        double a = atof(buffer);
+        fgets(buffer, MAX_LINE, fp);
+        double e = atof(buffer);
+        fgets(buffer, MAX_LINE, fp);
+        double i = atof(buffer);
+        fgets(buffer, MAX_LINE, fp);
+        double node = atof(buffer);
+        fgets(buffer, MAX_LINE, fp);
+        double omega = atof(buffer);
+        fgets(buffer, MAX_LINE, fp);
+        double M = atof(buffer);
+        double f = reb_tools_M_to_f(e, M);
+        struct reb_particle p = reb_tools_orbit_to_particle(1.,r->particles[0],0.,a,e,i,node,omega,f);
+        reb_add(r, p);
     }
+    fclose(fp);
+
     reb_move_to_com(r);
-    reb_integrate(r, INFINITY);
+    reb_integrate(r, 365.25*2.*M_PI);
 }
 
 void heartbeat(struct reb_simulation* r){
-    if (reb_output_check(r, 100.)){
-        reb_output_timing(r, INFINITY);
+    printf("%f\n",r->t);
+    if (reb_output_check(r, 1.00)){
+        printf("yee XD");
+        char* file = "testoutput.txt";
+        reb_output_ascii(r, file);
+        
     }
 }
 
